d390bcd7896ca92c09929829ac530fd138065caa
Michał Sidor (Michcioperz) <public+git@meekchopp.es>
2017-02-17 21:00:06 +0100

    Why not reinvent more wheels already?

--- libmprz.h
+++ libmprz.h
@@ -1,14 +1,19 @@
 #include <string>
 #include <stack>
-#include <cmath>
 
 namespace mprz {
-	std::string to_string(long long value) {
+	template<typename T>
+	T abs(T t) {
+		if (t < 0) return -t;
+		return t;
+	}
+	template<typename T>
+	std::string to_string(T value) {
 		if (value == 0) return "0";
 		bool sign = value < 0;
 		std::stack<char> digits;
 		do {
-			digits.push(std::abs(value % 10) + 48);
+			digits.push(abs(value % 10) + 48);
 			value /= 10;
 		} while (value != 0);
 		if (sign) digits.push('-');
