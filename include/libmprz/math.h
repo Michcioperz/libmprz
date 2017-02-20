7af49a2174e7f4df5225301f5172428cf47dc1dd
Michał Sidor (Michcioperz) <public+git@meekchopp.es>
2017-02-20 19:59:08 +0100

    TIL this stuff can't have split implementation.

--- include/libmprz/math.h
+++ include/libmprz/math.h
@@ -6,13 +6,33 @@
 #define LIBMPRZ_MATH_H
 
 #include <string>
+#include "stack.h"
 
 namespace mprz {
     template<typename T>
-    T abs(T t);
+    T abs(T value) {
+        if (value < 0)
+            return -value;
+        return value;
+    }
 
     template<typename T>
-    std::string to_string(T value);
+    std::string to_string(T value) {
+        if (value == 0) return "0";
+        bool sign = value < 0;
+        stack<char> digits;
+        do {
+            digits.push(abs(value % 10) + 48);
+            value /= 10;
+        } while (value != 0);
+        if (sign) digits.push('-');
+        std::string ret = "";
+        do {
+            ret += digits.top();
+            digits.pop();
+        } while (!digits.empty());
+        return ret;
+    }
 }
 
 
--- include/libmprz/stack.h
+++ include/libmprz/stack.h
@@ -11,18 +11,41 @@ namespace mprz {
     template<typename T>
     class stack {
     private:
-        struct element;
+        struct element {
+            element *parent;
+            T value;
+        };
         element *__top = nullptr;
     public:
-        bool empty();
+        bool empty() {
+            return __top == nullptr;
+        }
 
-        size_t count();
+        size_t count() {
+            element *ptr = __top;
+            size_t counter = 0;
+            while (ptr != nullptr) {
+                counter++;
+                ptr = ptr->parent;
+            }
+        }
 
-        T top();
+        T top() {
+            return __top->value;
+        }
 
-        void push(T value);
+        void push(T value) {
+            element *new_top = new element();
+            new_top->parent = __top;
+            new_top->value = value;
+            __top = new_top;
+        }
 
-        void pop();
+        void pop() {
+            element *old_top = __top;
+            __top = old_top->parent;
+            delete old_top;
+        }
     };
 }
 
