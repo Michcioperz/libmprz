d920c5f74e70620f23966f50c06d746ec03cc564
Michał Sidor (Michcioperz) <public+git@meekchopp.es>
2017-02-20 20:10:56 +0100

    So it turns out tests work now.

--- test/test_everything.cpp
+++ test/test_everything.cpp
@@ -3,16 +3,41 @@
 //
 
 #include <libmprz/math.h>
-#include "gtest/gtest.h"
+#include <libmprz/bell_utils.h>
+#include <gtest/gtest.h>
 
-TEST(MathAbsTest, ZeroNo) {
+TEST(MathAbs, Zero) {
     EXPECT_EQ(0, mprz::abs(0));
 }
 
-TEST(MathAbsTest, PositiveNumber) {
+TEST(MathAbs, PositiveNumber) {
     EXPECT_EQ(1, mprz::abs(1));
+}
+
+TEST(MathAbs, NegativeNumber) {
+    EXPECT_EQ(134, mprz::abs(-134));
 }
 
 TEST(ToString, PositiveNumber) {
     EXPECT_EQ("150", mprz::to_string(150));
+}
+
+TEST(ToString, NegativeNumber) {
+    EXPECT_EQ("-1337", mprz::to_string(-1337));
+}
+
+TEST(ToString, Zero) {
+    EXPECT_EQ("0", mprz::to_string(0));
+}
+
+TEST(DoesItRingABell, ObviousChar) {
+    EXPECT_EQ(true, mprz::does_it_ring_a_bell('\a'));
+}
+
+TEST(DoesItRingABell, Uwubuntu) {
+    EXPECT_EQ(false, mprz::does_it_ring_a_bell("uwubuntu"));
+}
+
+TEST(DoesItRingABell, LoudAsie) {
+    EXPECT_EQ(true, mprz::does_it_ring_a_bell("\asie\asie\asie\asie\asie\asie"));
 }
